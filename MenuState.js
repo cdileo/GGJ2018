cb29c54c65c229dae16f544da7474b0a47bf6367
Angela Chen <angelaichen87@gmail.com>
2018-01-28 14:21:20 -0800

    set music to play in menustate

--- MenuState.js
+++ MenuState.js
@@ -3,10 +3,15 @@ const MenuState = {
 
     preload : function () {
         game.load.image("splashScreen", "assets/submission/coverart.png");
+        game.load.audio('mainLoop', 'assets/audio/Angela ggj18 SV1 music loop.wav');
     },
+
+    music: null,
 
     create : function () {
         game.add.sprite(0, 0, "splashScreen");
+        this.music = game.add.audio('mainLoop');
+        this.music.play();
         this._createMenuText();
         this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
